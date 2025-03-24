8ec719f2be0c2d7cb45b718b9a1966aa5d54acff
Ankush Rathore <132293970+ankushrat@users.noreply.github.com>
2025-03-24 16:42:37 +0530

    Add files via upload
    no changes

---
+++ server.js
@@ -0,0 +1,18 @@
+const app = require("./app"); // Import Express app
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB Atlas
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+//key_id	key_secret
+//rzp_test_BzmOBgf80vVuZb	GQiOsMz9aCzVAgV6huFjzzpo
